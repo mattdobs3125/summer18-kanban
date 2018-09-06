c39b913e78cc5f0e1630e58234fd8692a0bfe4c3
Isaiah White <isaiahwhite208@gmail.com>
2018-09-06 13:45:39 -0600

    router test

--- client/src/router.js
+++ client/src/router.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// @ts-ignore
 import Boards from './views/Boards.vue'
+// @ts-ignore
 import Board from './views/Board.vue'
+// @ts-ignore
 import Login from './views/Login.vue'
+// @ts-ignore
+import List from './components/List.vue'
+// @ts-ignore
+import Task from './components/Task.vue'
 
 Vue.use(Router)
 
@@ -18,6 +25,16 @@ export default new Router({
       name: 'board',
       props: true,
       component: Board
+    }, {
+      path: '/list/:listId',
+      name: 'list',
+      props: true,
+      component: List
+    }, {
+      path: '/task/:taskId',
+      name: 'task',
+      props: true,
+      component: Task
     },
     {
       path: '/login',

0ff3cb51fde7cdcfc03c6ed0182b49ae2dc3b3b2
matt <matthew.dobrowolski5@gmail.com>
2018-09-06 10:47:00 -0600

    li

--- server/server-assets/models/list.js
+++ server/server-assets/models/list.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 let ObjectId = Schema.Types.ObjectId
 let schemaName = 'List'
-let shema = new Schema({
+let schema = new Schema({
     title:{type : String, required : true },
     boardId : { type : ObjectId , ref:'Board' , required : true},
     created: { type: Number, required: true, default: Date.now() },

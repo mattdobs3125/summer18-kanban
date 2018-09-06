db1c8621defdf60f62dbf8c845ec8e7ec1ebaf06
Isaiah White <isaiahwhite208@gmail.com>
2018-09-06 10:54:06 -0600

    tweaks

--- server/server-assets/routes/list.js
+++ server/server-assets/routes/list.js
@@ -1,45 +1,45 @@
 let router = require('express').Router()
 let list = require('../models/list')
 
-router.get('/:boardId',(req,res,next)=>{
-list.find({boardId: req.params.body})
-.then(data=>{
-    res.send(data)
-})
-.catch(err=>{
-    console.log(err)
-    next()
-})
+router.get('/:boardId', (req, res, next) => {
+    list.find({ boardId: req.params.body })
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            console.log(err)
+            next()
+        })
 })
 
 
 
-router.post('/',(req,res,next)=>{
+router.post('/', (req, res, next) => {
     req.body.authorId = req.session.uid
     releaseEvents.body.boardId = req.params.boardId
     list.create(req.body)
-    .then(newList=>{
-        res.send(newList)
-    })
-    .catch(err=>{
-    console.log(err)
-    next()
-    }
-    )
+        .then(newList => {
+            res.send(newList)
+        })
+        .catch(err => {
+            console.log(err)
+            next()
+        }
+        )
 })
-router.put('/:id',(req,res,next)=>{
+router.put('/:id', (req, res, next) => {
     list.findByIdAndUpdate(req.params.id, req.body)
-    .then(()=>res.send('success'))
+        .then(() => res.send('success'))
 })
 
 
-router.delete('/:id',(req,res,next)=>{
+router.delete('/:id', (req, res, next) => {
     list.findById(req.params.id)
-    .then(list=>{
-    lit.findByIdAndRemove(req.params.id)
-    .then(data=>{
-        res.send('deleted')
-    })  
-    })
+        .then(list => {
+            list.findByIdAndRemove(req.params.id)
+                .then(data => {
+                    res.send('deleted')
+                })
+        })
 })
 module.exports = router

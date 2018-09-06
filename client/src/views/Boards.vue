8264819b88bb81c7b446be02084cbf909ed405d4
Isaiah White <isaiahwhite208@gmail.com>
2018-09-06 11:02:23 -0600

    minor

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -8,7 +8,7 @@
     </form>
     <div v-for="board in boards" :key="board._id">
       <router-link :to="{name: 'board', params: {boardId: board._id}}">{{board.title}}</router-link>
-      <button @click="deleteBoard(board._id)">DELETE BOARD</button>
+      <button @click="deleteBoard(board._id)">Delete Board</button>
     </div>
   </div>
 </template>
